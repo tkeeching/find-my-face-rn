02af3a8bb5f9bdc154375d7177b7168ef2fb5069
Kee Tee <tkeeching@gmail.com>
2022-12-18 16:39:19 +1100

    setup

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, TextInput, View } from "react-native";
+import GlobalStyle from './style';
+
+const App = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <View style={GlobalStyle.container}>
+      <Text>Open up App.js to start working on your app!!</Text>
+      <Text>Hi,</Text>
+      <TextInput
+        style={GlobalStyle.textinput}
+        placeholder="Please enter your name"
+        onChangeText={(input) => setName(input)}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
---
+++ style.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textinput: {
+    height: 60,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 10,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
